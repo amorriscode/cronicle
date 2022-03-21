5d6ac97cabf50956fe1b5bf00624ee4eb96506c9
Alice Zhao <azhao991@gmail.com>
2022-03-20 18:00:39 -0700

    feat: add delete todo

--- cmd/todo/delete/delete.go
+++ cmd/todo/delete/delete.go
@@ -1,7 +1,12 @@
 package delete
 
 import (
+	"cronicle/ui/constants"
+	"cronicle/utils"
+	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
@@ -14,12 +19,23 @@ func New() *cobra.Command {
 		Run:   run,
 	}
 
-	cmd.Flags().StringP("number", "n", "", "number on ordered list to delete")
+	cmd.Flags().IntP("number", "n", 0, "number on ordered list to delete")
 	cmd.MarkFlagRequired("number")
 
 	return cmd
 }
 
 func run(cmd *cobra.Command, args []string) {
-	log.Println("Deleting a todo doc...")
+	n, _ := cmd.Flags().GetInt("number")
+	if n == 0 {
+		return
+	}
+	dirPath := utils.GetPath([]string{"todo"})
+	files, _ := ioutil.ReadDir(dirPath)
+	e := os.Remove(filepath.Join(dirPath, files[n-1].Name()))
+
+	if e != nil {
+		log.Fatal(constants.ERROR_DELETE_FILE, e)
+	}
+
 }
